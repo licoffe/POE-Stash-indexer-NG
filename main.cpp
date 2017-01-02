39f35401efce31fb5b89866e8a9d57f33116c5b9
Licoffe <p1lgr11m@gmail.com>
2017-01-02 13:48:53 +0100

    include fix

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@
 #include <cppconn/resultset.h>
 #include <cppconn/statement.h>
 #include <cppconn/prepared_statement.h>
-#include "./include/rapidjson/document.h"
+#include "rapidjson/document.h"
 #include "Misc.h"
 #include "Colors.h"
 #include "CFG_reader.h"
